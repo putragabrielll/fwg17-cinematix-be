9e3917331692700d64cac09f57e6aee7ecd42c3e
putragabrielll <putragmahk@gmail.com>
2024-03-11 17:31:45 +0700

    fix: unit test

---
+++ testing/routers_test.go
@@ -0,0 +1,30 @@
+package testing
+
+import (
+	"net/http"
+	// "net/http/httptest"
+	"reflect"
+	"testing"
+
+	// "github.com/gin-gonic/gin"
+	// customerControllers "github.com/putragabrielll/fwg17-cinematix-be/src/controllers/customer"
+	// "github.com/putragabrielll/fwg17-cinematix-be/src/lib" 
+	"github.com/putragabrielll/fwg17-cinematix-be/src/services"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListAllMovies(t *testing.T) {
+	// db := lib.DbConnection()
+
+	// c := gin.Context{}
+
+	req, _ := http.NewRequest("GET", "/movies", nil)
+
+	// rr := httptest.NewRecorder()
+	// customerControllers.ListAllMovies(c, req)
+
+	// if reflect.TypeOf(req) != reflect.TypeOf(&services.ResponseList{}) {
+	// 	t.Error("failed test")
+	// }
+	assert.Equal(t, reflect.TypeOf(&services.ResponseList{}), reflect.TypeOf(req), "failed test")
+}
